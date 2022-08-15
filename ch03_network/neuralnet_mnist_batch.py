network inferrence, batch , fix bug(softmax)

ch03_network/neuralnet_mnist_batch.py
@@ -11,7 +11,7 @@ def get_data():
     return x_test, t_test
 
 def init_network():
-    with open("./sample_weight.pkl", 'rb') as f:
+    with open("sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
 
@@ -32,16 +32,15 @@ x, t = get_data()
 network = init_network()
 
 
+batch_size = 100
 accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)
-    if p == t[i]:
-        accuracy_cnt += 1
 
-print(float(accuracy_cnt))
-print(len(x))
+for i in range(0, len(x), batch_size):
+    x_batch = x[i: i + batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis = 1)
+    accuracy_cnt += np.sum(p == t[i : i + batch_size])
+
 print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
-
 
 

common/functions.py
@@ -7,11 +7,11 @@ def identity_function(x):
 def step_function(x):
     return np.array(x > 0, dtype = np.int)
 
-def sigmod(x):
+def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmod_grad(x):
-    return (1 - sigmod(x)) * sigmod(x)
+def sigmoid_grad(x):
+    return (1 - sigmoid(x)) * sigmoid(x)
    
 def relu(x):
     return np.maximum(0, x)
@@ -21,6 +21,7 @@ def softmax(x):
         x = x.T
         x = x - np.max(x, axis = 0)
         y = np.exp(x) / np.sum(np.exp(x), axis = 0)
+        return y.T 
 
     x =x - np.max(x)
     return np.exp(x) / np.sum(np.exp(x))